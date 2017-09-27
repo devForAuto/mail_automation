eda3766b68b697ee3ae543105dbab21abaaa01a6
devForAuto <32116641+devForAuto@users.noreply.github.com>
2017-09-27 15:28:19 +0800

    修改配置方法及删除冗余数据

--- run_case.py
+++ run_case.py
@@ -16,14 +16,14 @@ import time
 
 if __name__ == '__main__':
     now = time.strftime("%Y-%m-%d %H-%M-%S")
-    filename = './report/' + now + 'result.html'
+    filename = './report/' + now + '_result.html'
     print(filename)
     fp = open(filename, 'wb')
     runner = HTMLTestRunner(stream=fp,
                             title='客服邮箱管理系统',
                             description=u'环境：windows 10 浏览器:chrome')
     discover = unittest.defaultTestLoader.discover('./test_case',
-                                                   pattern='M_C_002*.py')
+                                                   pattern='M_C_0033*.py')
     runner.run(discover)
     fp.close()
 
--- test_case/models/base_conf.py
+++ test_case/models/base_conf.py
@@ -11,14 +11,22 @@ from __future__ import print_function
 
 """
 
-import sys
 import configparser
-
+import os
+import sys
+import platform
 
 class ConSys(object):
     def cons(self):
+        if platform.system() == 'Windows':
+            root_dir = '/'.join(os.path.realpath(__file__).split('\\')[:-3])
+        else:
+            root_dir = '/'.join(os.path.realpath(__file__).split('/')[:-1])
+        sys.path.append(root_dir)
+        print(root_dir)
+        config_file = os.path.join(root_dir, 'config/system.cfg')
         config = configparser.ConfigParser()
-        config_file = 'F:/mail_automation/config/system.cfg'
+        # config_file = './config/system.cfg'
         with open(config_file, 'r') as cfg_file:
             config.readfp(cfg_file)
             name = config.get('info', 'host')
